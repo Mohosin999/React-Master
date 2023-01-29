63d53af3de7255a79cb414b8875fbc461f7a1a0f
Mohosin999 <hasanmohosin998@gmail.com>
2023-01-29 16:05:48 +0600

    mongoose connection completed

--- iNoteBook-Project/Backend/server.js
+++ iNoteBook-Project/Backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const connectDB = require("./db");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Something Send");
 });
 
-app.listen(8000, () => {
-  console.log("Server is listening on PORT 8000");
-});
+connectDB("mongodb://127.0.0.1:27017/iNoteBook")
+  .then(() => {
+    console.log("Database Connected");
+
+    app.listen(8000, () => {
+      console.log("Server is listening on PORT 8000");
+    });
+  })
+  .catch((e) => console.log(e.message));

43b30d9978400cbde83f1a42b0c1f871c76026e9
Mohosin999 <hasanmohosin998@gmail.com>
2023-01-29 09:05:04 +0600

    enable dark and white mode

--- text-utils/src/App.jsx
+++ text-utils/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import About from "./components/about/About";
 import Navbar from "./components/navbar/Navbar";
-import TextForm from "./components/text-form/TextForm";
+// import TextForm from "./components/text-form/TextForm";
 
 const App = () => {
   return (
     <>
       <Navbar title={"TextUtils"} />
       <div className="container mt-4">
-        <TextForm header={`Write What's on Your Mind🤓`} />
+        {/* <TextForm header={`Write What's on Your Mind🤓`} /> */}
+        <About />
       </div>
     </>
   );

69c09112cf4524179d2ab99f4f320d8b3b64a8ab
Mohosin999 <hasanmohosin998@gmail.com>
2023-01-29 10:31:09 +0600

    simple practice project

--- text-utils/src/App.jsx
+++ text-utils/src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import About from "./components/about/About";
 import Navbar from "./components/navbar/Navbar";
-// import TextForm from "./components/text-form/TextForm";
+import TextForm from "./components/text-form/TextForm";
 
 const App = () => {
   return (
     <>
       <Navbar title={"TextUtils"} />
       <div className="container mt-4">
-        {/* <TextForm header={`Write What's on Your Mind🤓`} /> */}
-        <About />
+        <TextForm header={`Write What's on Your Mind🤓`} />
+        <div className="pt-5 pb-5">
+          <About />
+        </div>
       </div>
     </>
   );

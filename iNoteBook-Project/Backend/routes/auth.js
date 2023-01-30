8e0b8eecd05cbc7a329ad7edf67be9d6f2868bcc
Mohosin999 <programmermohosin@gmail.com>
2023-01-30 08:48:59 +0600

    stop here this project, i will come back soon

--- iNoteBook-Project/Backend/routes/auth.js
+++ iNoteBook-Project/Backend/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 // create a user using : POST "/api/auth", doesn't require auth
 router.post("/", (req, res) => {
